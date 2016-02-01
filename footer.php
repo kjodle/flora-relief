859607e5a1bc4c026d0dc2aafd713a68e4a803f3
kjodle <kenneth.odle@gmail.com>
2016-02-01 17:23:41 -0500

    Initial commit

---
+++ footer.php
@@ -0,0 +1,9 @@
+</div><?php //closing for #main?>
+<div id="footer">
+
+
+	<p><span>Powered by <a href="http://wordpress.org/" title="WordPress">WordPress</a> and <a href="http://designer-relief.com">Designer Relief</a></span><strong><?php bloginfo('name');?></strong> Copyright &copy; <?php echo date('Y');?> All Rights Reserved.</p>
+</div>
+<?php wp_footer();?>
+</body>
+</html>
